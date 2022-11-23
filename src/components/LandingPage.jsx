aed558fc20c8ed1bd31983329f6a69dc90fd8447
Zachary Soliman <zachsoliman1@gmail.com>
2022-11-23 15:55:31 -0500

    landing page text

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,10 +5,22 @@ const LandingPage = () => {
         <div className="landingpage">
             <Navbar />
 
-            <h1
+            {/* <h1
                 style={{
                     margin: '0', padding: '1.5%'
-                }}>Sabrinas Pandas</h1>
+                }}>Sabrinas Pandas</h1> */}
+
+            <div className="landingText">
+
+                <h1>World Class Artists</h1>
+                <p>Sign up to keep updated with
+                    Gallery openings
+                    and
+                    our projects
+                </p>
+            </div>
+
+            <button>Join us ></button>
 
         </div>
     )
