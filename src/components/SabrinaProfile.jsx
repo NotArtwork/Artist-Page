54c19c1959ac86d8955e82801b9f271f128335ff
Zachary Soliman <zachsoliman1@gmail.com>
2022-11-30 18:13:36 -0500

    sendContact

--- src/components/SabrinaProfile.jsx
+++ src/components/SabrinaProfile.jsx
@@ -6,7 +6,7 @@ const SabrinaProfile = () => {
             <Navbar />
             <div className="profileDiv">
 
-                <h1>Sabrina</h1>
+                <h1 className="profileName">Sabrina</h1>
                 <img className="profileImg" src="https://pbs.twimg.com/profile_images/646496617525288960/kNke3rKq_400x400.jpg" />
                 <p className="profileBio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam numquam distinctio temporibus inventore, obcaecati odio! Incidunt et iste quo rerum, ullam autem eum placeat explicabo animi accusantium velit rem tenetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque molestiae sequi rerum delectus corrupti laboriosam in qui ad earum perspiciatis, assumenda quis quidem. Ut sunt similique expedita et labore dignissimos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam mollitia eum sunt animi ad fugiat? Fuga vero perspiciatis nulla provident voluptate quasi, sunt voluptatum aliquam, quibusdam architecto molestiae porro quaerat.</p>
 
