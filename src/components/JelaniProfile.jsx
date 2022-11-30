16fc1c2b596708c99955563b2661008c83faec3e
Zachary Soliman <zachsoliman1@gmail.com>
2022-11-30 16:50:51 -0500

    profile components

---
+++ src/components/JelaniProfile.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+
+const JelaniProfile = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Jelani</h1>
+            <img className="profileImg" src="https://media.istockphoto.com/id/1389097654/photo/young-panda-climbing-a-tree-under-the-supervision-of-his-mother.jpg?b=1&s=170667a&w=0&k=20&c=c0nBeVG29cs7pHK7s4iFa5evHSS0zOcTibY4OeGlhqs=" />
+            <p className="profileBio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam numquam distinctio temporibus inventore, obcaecati odio! Incidunt et iste quo rerum, ullam autem eum placeat explicabo animi accusantium velit rem tenetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque molestiae sequi rerum delectus corrupti laboriosam in qui ad earum perspiciatis, assumenda quis quidem. Ut sunt similique expedita et labore dignissimos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam mollitia eum sunt animi ad fugiat? Fuga vero perspiciatis nulla provident voluptate quasi, sunt voluptatum aliquam, quibusdam architecto molestiae porro quaerat.</p>
+        </div>
+    )
+}
+
+export default JelaniProfile;
