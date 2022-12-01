595817cf21490d0ebdf1ad68429e9b0985bc2620
NotArtwork <AntArce3@gmail.com>
2022-12-01 10:50:03 -0500

    added auth

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,7 +9,7 @@ router.get('/', UserController.index)
 router.post('/show', UserController.show)
 router.post('/store', upload.single('profile_image'), UserController.store)
 router.patch('/update/pieces', upload.array('art_pieces'), UserController.update)
-router.post('/update/profile_image', upload.single('profile_image'), UserController.update)
+router.patch('/update/profile_image', upload.single('profile_image'), UserController.update)
 router.post('/delete', UserController.destroy)
 
 module.exports = router 
